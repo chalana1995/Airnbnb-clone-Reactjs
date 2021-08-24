70f67d5e308612a64e0787f0cb0aa11740ce424d
chalana kalpitha <chalanakalpitha72@gmail.com>
2021-08-24 19:42:01 +0530

    medium card part finish

--- airbnb-clone/pages/index.js
+++ airbnb-clone/pages/index.js
@@ -1,9 +1,10 @@
 import Head from 'next/head'
 import Banner from '../components/Banner'
 import Header from '../components/Header'
+import MediumCard from '../components/MediumCard';
 import SmallCard from '../components/SmallCard';
 
-export default function Home({ exploreData }) {
+export default function Home({ exploreData, cardData }) {
   return (
     <div className="">
       <Head>
@@ -34,6 +35,19 @@ export default function Home({ exploreData }) {
             }
           </div>
         </section>
+
+        <section>
+          <h2 className="text-4xl font-semibold py-8">Live Anywhere</h2>
+          <div className="flex space-x-3 overflow-scroll scrollbar-hide p-3 -ml-3">
+            {cardData?.map((item) => (
+              <MediumCard
+                key={item.img}
+                img={item.img}
+                title={item.title}
+              />
+            ))}
+          </div>
+        </section>
       </main>
     </div>
   )
@@ -43,9 +57,12 @@ export default function Home({ exploreData }) {
 export async function getStaticProps() {
   const exploreData = await fetch('https://links.papareact.com/pyp').then(res => res.json());
 
+  const cardData = await fetch('https://links.papareact.com/zp1').then(res => res.json());
+
   return {
     props: {
-      exploreData
+      exploreData,
+      cardData
     }
   }
 }
