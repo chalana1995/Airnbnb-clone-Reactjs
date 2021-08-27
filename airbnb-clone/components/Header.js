74e59a54d0ded992d2e610456f070dff823627bf
chalana kalpitha <chalanakalpitha72@gmail.com>
2021-08-27 22:15:30 +0530

    calandar part done

--- airbnb-clone/components/Header.js
+++ airbnb-clone/components/Header.js
@@ -1,10 +1,26 @@
 import Image from 'next/image';
 import { SearchIcon, GlobeAltIcon, UserIcon, UserCircleIcon, MenuIcon } from '@heroicons/react/solid'
 import { useState } from 'react';
+import 'react-date-range/dist/styles.css'; // main style file
+import 'react-date-range/dist/theme/default.css'; // theme css file
+import { DateRangePicker } from 'react-date-range';
 
 function Header() {
 
-    const [searchInput, setSearchInput] = useState('')
+    const [searchInput, setSearchInput] = useState('');
+    const [startDate, setStartDate] = useState(new Date());
+    const [endDate, setEndDate] = useState(new Date());
+
+    const handleSelect = (ranges) => {
+        setStartDate(ranges.selection.startDate)
+        setEndDate(ranges.selection.endDate)
+    }
+
+    const selectionRange = {
+        startDate: startDate,
+        endDate: endDate,
+        key: 'selection',
+    }
 
     return (
         <header className="sticky top-0 z-50 grid grid-cols-3 bg-white shadow-md p-5 md:px-10">
@@ -38,6 +54,19 @@ function Header() {
                     <UserCircleIcon className="h-6" />
                 </div>
             </div>
+
+            {
+                searchInput && (
+                    <div className="flex flex-col col-span-3 mx-auto">
+                        <DateRangePicker
+                            ranges={[selectionRange]}
+                            minDate={new Date()}
+                            rangeColors={["#FD5B61"]}
+                            onChange={handleSelect}
+                        />
+                    </div>
+                )
+            }
         </header>
     )
 }
