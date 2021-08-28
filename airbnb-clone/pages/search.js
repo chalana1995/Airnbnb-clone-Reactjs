aecf2c9b6b7fd5d1d4b1ea63600c84ca1159a9a2
chalana kalpitha <chalanakalpitha72@gmail.com>
2021-08-28 12:22:53 +0530

    today air bnb part finish

--- airbnb-clone/pages/search.js
+++ airbnb-clone/pages/search.js
@@ -1,7 +1,16 @@
+import Header from '../components/Header';
+import Footer from '../components/Footer';
+
 function Search() {
     return (
         <div>
-            
+            <Header />
+            <main>
+                <section>
+                    <p>300+ Stays for 5 nimbers of guest</p>
+                </section>
+            </main>
+            <Footer />
         </div>
     )
 }
